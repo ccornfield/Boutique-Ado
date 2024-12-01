299fec2b80b67577d567197a3ebed124ef1a1654
Charles Cornfield <ecolus48@gmail.com>
2024-12-01 16:11:08 +0000

    Implemented stripe.

--- checkout/views.py
+++ checkout/views.py
@@ -12,7 +12,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QRFMWFJ35E3JzrfyqFA2epun0S0QDJwANBr4mVPPSoAInEx6rXvhnYpA99oxRX66dWjWox1dQvZZrJvxF6NjWQs000jG70H0K',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
