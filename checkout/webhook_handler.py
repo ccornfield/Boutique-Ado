b83714016da277ae8d2e269fd203a5ffbead9e97
Charles Cornfield <ecolus48@gmail.com>
2024-12-06 16:27:08 +0000

    Stripe 15 started. Webhooks not being called.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -7,17 +7,21 @@ class StripeWH_Handler:
 
     def handle_event(self, event):
         return HttpResponse(
-            content = f'webhook received: {event["type"]}',
+            content = f'Unhandled webhook received: {event["type"]}',
             status=200)
     
     def handle_payment_intent_succeeded(self, event):
+
+        intent = event.data.object
+        print(intent)
+
         return HttpResponse(
             content = f'webhook received: {event["type"]}',
             status=200) 
     
     def handle_payment_intent_payment_failed(self, event):
         return HttpResponse(
-            content = f'Payment Failed webhook received: {event["type"]}',
+            content = f'webhook received: {event["type"]}',
             status=200) 
 
     
